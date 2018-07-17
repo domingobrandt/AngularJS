040e4d5b54a04e1853d31b58d8693fb10fb8ed03
Domingo Brandt <brandtyet02@gmail.com>
2018-07-17 19:32:10 -0400

    Components Part 1

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,11 @@
+export class Dish { 
+    id: number;
+    name: string;
+    image: string;
+    category: string;
+    label: string;
+    price: string;
+    //featured: boolean;
+    description: string;
+   // comments: Comment[];
+}
