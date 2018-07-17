67196b91e929aa0dafc5c0caa9847998518146df
Domingo Brandt <brandtyet02@gmail.com>
2018-07-16 23:16:44 -0400

    update Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatToolbarModule, /*MatDialogModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule,
+  MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule,
+MatSlideToggleModule,MatProgressSpinnerModule*/} from '@angular/material'; 
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
