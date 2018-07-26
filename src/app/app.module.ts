59bd7a99795220d6daa97cc2f78f55534de0d531
Domingo Brandt <brandtyet02@gmail.com>
2018-07-25 23:47:07 -0400

    Angular Services, Routing and Single Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { DishService } from './services/dish.service';
 import { PromotionService } from "./services/promotion.service";
+import { LeaderService } from "./services/leader.service";
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
@@ -43,7 +44,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatListModule,MatButtonModule,MatCardModule,MatGridListModule,MatIconModule,
     AppRoutingModule,
   ],
-  providers: [DishService,PromotionService],
+  providers: [DishService,PromotionService,LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
