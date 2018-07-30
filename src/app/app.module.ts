e142d1e2f0fc750a5206530733a8769169c67ca2
Domingo Brandt <brandtyet02@gmail.com>
2018-07-29 23:20:12 -0400

    Template Forms Part 1 login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import {MatToolbarModule, /*MatDialogModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule,
-  MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule,
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+import {MatToolbarModule,/* , MatDatepickerModule, ,
+  , MatRadioModule, MatSelectModule, MatSliderModule,
 MatSlideToggleModule,MatProgressSpinnerModule*/} from '@angular/material'; 
 import {MatIconModule} from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -24,6 +28,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +39,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -43,8 +49,13 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatToolbarModule, 
     MatListModule,MatButtonModule,MatCardModule,MatGridListModule,MatIconModule,
     AppRoutingModule,
+    MatDialogModule,
+    FormsModule,MatFormFieldModule,MatInputModule,MatCheckboxModule
   ],
   providers: [DishService,PromotionService,LeaderService],
+  entryComponents:[
+    LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
